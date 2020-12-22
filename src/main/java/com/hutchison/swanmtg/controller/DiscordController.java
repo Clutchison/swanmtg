dc8225ca32d818ad50d1f23ce86944209415da26
Sean Hutchison <xskhutchx@gmail.com>
2020-12-22 08:41:34 -0700

    comment

--- src/main/java/com/hutchison/swanmtg/controller/DiscordController.java
+++ src/main/java/com/hutchison/swanmtg/controller/DiscordController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DiscordController {
 
     @GetMapping
+    // test
     public String ping() {
         return "pong!";
     }
